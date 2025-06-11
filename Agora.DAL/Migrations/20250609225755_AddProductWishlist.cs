2f6993ccaa1ced36ebf04873d099f6eccc141199
IrynaKonovalowa <iri.moummi@gmail.com>
2025-06-11 18:37:59 +0200

    Task146 ProductWishlist entity added. Product, Wishlist entities changed.

---
+++ Agora.DAL/Migrations/20250609225755_AddProductWishlist.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agora.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductWishlist : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductWishlist");
+
+            migrationBuilder.CreateTable(
+                name: "ProductWishlists",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    WishlistId = table.Column<int>(type: "int", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductWishlists", x => new { x.ProductId, x.WishlistId });
+                    table.ForeignKey(
+                        name: "FK_ProductWishlists_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductWishlists_Wishlists_WishlistId",
+                        column: x => x.WishlistId,
+                        principalTable: "Wishlists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductWishlists_WishlistId",
+                table: "ProductWishlists",
+                column: "WishlistId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductWishlists");
+
+            migrationBuilder.CreateTable(
+                name: "ProductWishlist",
+                columns: table => new
+                {
+                    ProductsId = table.Column<int>(type: "int", nullable: false),
+                    WishlistsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductWishlist", x => new { x.ProductsId, x.WishlistsId });
+                    table.ForeignKey(
+                        name: "FK_ProductWishlist_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductWishlist_Wishlists_WishlistsId",
+                        column: x => x.WishlistsId,
+                        principalTable: "Wishlists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductWishlist_WishlistsId",
+                table: "ProductWishlist",
+                column: "WishlistsId");
+        }
+    }
+}
