cb74af527a2cdee4527e8f4719eea603e5dae08d
Nastia <nastasia.schull@gmail.com>
2025-06-14 16:45:13 +0200

    Task153 Added translation API and multilingual product search

---
+++ Agora.WebApi/Controllers/LanguageController.cs
@@ -0,0 +1,30 @@
+﻿using Agora.BLL.Interfaces;
+using Agora.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Agora.Controllers
+{
+    [ApiController]
+    [Route("api/translate")]
+    public class LanguageController : ControllerBase
+    {
+        private readonly ITranslationService _translationService;
+
+        public LanguageController(ITranslationService translationService)
+        {
+            _translationService = translationService;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Translate([FromBody] TranslateRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Text))
+                return BadRequest("Text is required");
+
+            var translated = await _translationService.Translate(request.Text, request.Locale);
+            return Ok(new { translatedText = translated });
+        }
+    }
+}
---
+++ Agora.WebApi/Models/TranslateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Agora.Models
+{
+    public class TranslateRequest
+    {
+        public string Text { get; set; } = string.Empty;
+        public string Locale { get; set; } = "en";
+    }
+}
