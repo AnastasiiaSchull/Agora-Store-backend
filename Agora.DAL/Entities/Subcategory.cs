1ae7b50167a2f656e2ff49a1f1aef5f631a160d0
Nastia <nastasia.schull@gmail.com>
2025-07-03 18:34:42 +0200

    Task168 fix: make Subcategory.CategoryId nullable to avoid migration error

--- Agora.DAL/Entities/Subcategory.cs
+++ Agora.DAL/Entities/Subcategory.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
 
         public virtual Category? Category { get; set; }
         public virtual ICollection<Product>? Products { get; set; }
