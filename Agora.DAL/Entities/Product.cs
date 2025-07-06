7d296985d0b0c23f98142ac45f4c5a6f8537e1d6
Nastia <nastasia.schull@gmail.com>
2025-07-06 18:18:46 +0200

    Task172 feat(product): add DiscountedPrice field to model and database (with migration)

--- Agora.DAL/Entities/Product.cs
+++ Agora.DAL/Entities/Product.cs
@@ -8,20 +8,17 @@ namespace Agora.DAL.Entities
         public string? Name { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
+        public decimal? DiscountedPrice { get; set; }
         public int StockQuantity { get; set; }
         public decimal Rating { get; set; }
         public string? ImagesPath { get; set; }
         public bool IsAvailable {  get; set; }
-
         public virtual int? SubcategoryId{ get; set; }
         public virtual Subcategory? Subcategory { get; set; }
-
         public virtual int? CategoryId{ get; set; }
         public virtual Category? Category { get; set; }
-
         public virtual int? BrandId { get; set; }
         public virtual Brand? Brand { get; set; }
-
         public virtual int? StoreId { get; set; }
         public virtual Store? Store { get; set; }
         public int? DiscountId { get; set; }
---
+++ Agora.DAL/Migrations/20250706082105_AddDiscountedPriceToProduct.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agora.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiscountedPriceToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountedPrice",
+                table: "Products",
+                type: "decimal(65,30)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountedPrice",
+                table: "Products");
+        }
+    }
+}
