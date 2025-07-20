8dcf911c9f86625885965cd3da586776d8b3bcda
Vladyslav <vlad.serdyuk.3456@gmail.com>
2025-07-20 19:15:21 +0300

    Task188 Chat client and chat admin implemented

---
+++ Agora.WebApi/Hubs/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using Agora.BLL.DTO;
+using Agora.BLL.Storages;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Agora.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string sender, string message)
+        {
+            var newMessage = new Message
+            {
+                Sender = sender,
+                MessageText = message,
+                SentAt = DateTime.UtcNow
+            };
+
+            ChatStorage.Messages.Add(newMessage);
+
+            await Clients.All.SendAsync("ReceiveMessage", newMessage.Sender, newMessage.MessageText, newMessage.SentAt);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            foreach (var msg in ChatStorage.Messages)
+            {
+                await Clients.Caller.SendAsync("ReceiveMessage", msg.Sender, msg.MessageText, msg.SentAt);
+            }
+
+            await base.OnConnectedAsync();
+        }
+    }
+}
